e070f815d0031e3bcf61c55d964e7ea1fb9f8986
Mika Vesasto <locheed@gmail.com>
2018-03-19 12:46:58 +0200

    Added precompiled version of parser.js

--- src/tools/fetchIltalehti.js
+++ src/tools/fetchIltalehti.js
@@ -1,7 +1,8 @@
-import Parser from "rss-parser";
-
+// import Parser from "rss-parser";
+/* global RSSParser */
+import "rss-parser/dist/rss-parser.min.js";
 export default async () => {
-  let parser = new Parser();
+  let parser = new RSSParser();
   let feed = await parser.parseURL("http://www.iltalehti.fi/rss/uutiset.xml");
   console.log(feed);
 
