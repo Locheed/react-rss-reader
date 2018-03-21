ae35d9a065a72dffde8223403decb14a6f3ebf3a
Mika Vesasto <locheed@gmail.com>
2018-03-21 15:04:35 +0200

    Added test to confirm onclick event for loading news detail

--- src/App.test.js
+++ src/App.test.js
@@ -5,10 +5,13 @@ import NewsList from "./containers/NewsList";
 import NewsDetail from "./components/NewsDetail";
 import Settings from "./components/Settings";
 
+const options = {
+  disableLifecycleMethods: true
+};
 describe("<App />", () => {
   let wrapper;
   beforeEach(() => {
-    wrapper = shallow(<App />);
+    wrapper = shallow(<App />, options);
   });
 
   it("renders NewsList", () => {
@@ -19,5 +22,22 @@ describe("<App />", () => {
   });
   it("renders Settings", () => {
     expect(wrapper.find(Settings).length).toEqual(1);
+  });
+
+  it("calls onClick event on click of a news title", () => {
+    const items = [
+      {
+        title: "Henkilöauto törmäsi kuorma-auton keulaan Liperissä"
+      }
+    ];
+    const onClick = jest.fn();
+    let newsList = mount(<NewsList items={items} onNewsSelect={onClick} />);
+    newsList
+      .find(".news-item li")
+      .first()
+      .simulate("click");
+    expect(onClick).toBeCalledWith({
+      title: "Henkilöauto törmäsi kuorma-auton keulaan Liperissä"
+    });
   });
 });
